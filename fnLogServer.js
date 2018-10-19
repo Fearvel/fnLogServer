8a121e6f81ffbf4c36a2f51ed889ef3512e622cd
Andreas Schreiner <schreiner.andreas@outlook.com>
2018-10-19 07:33:48 +0200

    0.0.0.3

--- fnLogServer.js
+++ fnLogServer.js
@@ -13,27 +13,34 @@ var server = http.createServer(function (req, res) {
 
 
     if (req.method === "POST") {
-
         var body = "";
         req.on("data", function (chunk) {
             body += chunk;
+            console.log(body);
+
         });
+
 
         req.on("end", function () {
             res.writeHead(200, {"Content-Type": "text/html"});
             var obj = JSON.parse(TransformToJson(body));
-            con.query('Insert into fnLog.Log (ProgramName,Guid, Title,Description, LogType) values (?,?,?,?,?)', ['test', obj.GUID, obj.Title, obj.Description, parseInt(obj.LogType)], function (error, results, fields) {
+            con.query('Insert into fnLog.Log (ProgramName,Guid, Title,Description, LogType) values (?,?,?,?,?)', [obj.ProgramName, obj.GUID, obj.Title, obj.Description, parseInt(obj.LogType)], function (error, results, fields) {
                 if (error) {
-                    res.end("False");
-                } else {
-                    res.end("True");
-                }
+                    res.end(CreateAnswer(false));
+                    console.log(CreateAnswer(false));
 
+                } else {
+                    res.end(CreateAnswer(true));
+                    console.log(CreateAnswer(true));
+
+                }
             });
         });
     }
     else {
-        res.end("False");
+        res.end(CreateAnswer(false));
+        console.log(CreateAnswer(false));
+
     }
 
 }).listen(8888);
@@ -48,8 +55,19 @@ function TransformToJson(s) {
     }
     s = s + "\"\n}";
     return s;
-
 }
+
+function CreateAnswer(b){
+    var s = "{\"result\": ";
+    if (b){
+        s+= "\"True\"";
+    } else{
+        s+= "\"False\"";
+    }
+    s+= "}";
+    return s;
+}
+
 
 
 function ConnectToMysql() {
